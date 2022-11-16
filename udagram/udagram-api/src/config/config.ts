9ba7bd691a6021a3ab6d969f5c50a08730aaeab3
Magdy156 <magdyhamdy156@gmail.com>
2022-11-16 07:16:02 +0200

    23

--- udagram/udagram-api/src/config/config.ts
+++ udagram/udagram-api/src/config/config.ts
@@ -15,7 +15,7 @@ export const config = {
   port: Number(process.env.PORT),
   aws_region: process.env.AWS_REGION,
   aws_profile: process.env.AWS_PROFILE,
-  aws_media_bucket: "bucketsmag",
+  aws_media_bucket: process.env.AWS_BUCKET,
   url: process.env.URL,
   jwt: {
     secret: process.env.JWT_SECRET,
