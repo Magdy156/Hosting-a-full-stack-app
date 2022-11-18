eb12a88da7782755832ce64822f679510676c512
Magdy156 <magdyhamdy156@gmail.com>
2022-11-18 07:13:27 +0200

    30

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://udagram-api-dev.eba-ms3989gh.us-east-1.elasticbeanstalk.com/api/v0",
+  apiHost: "http://udagram-api-dev.eba-m8m5r3bz.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
