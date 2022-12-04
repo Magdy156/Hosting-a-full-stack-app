d84eea16598bb704012221fd11febfb3fb9c2896
Magdy156 <magdyhamdy156@gmail.com>
2022-12-04 14:59:13 +0200

    336

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://udagram-api-dev.eba-rcxjejvx.us-east-1.elasticbeanstalk.com/api/v0'
+  apiHost: 'http://udagram-api-dev.eba-35jnf8kw.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
